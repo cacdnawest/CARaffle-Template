07ba61f0a02bde7c51a280414342e5f9a0c70c05
cacdnawest <robert.karlin@ca.com>
2017-08-29 23:30:27 -0500

    Update axa.php

--- plugins/axa.php
+++ plugins/axa.php
@@ -1,6 +1,6 @@
-<script type="text/javascript" id="ca_eum_ba" agent="browser" async
+<script type="text/javascript" id="ca_eum_ba" agent=browser
         src="https://cloud.ca.com/mdo/v1/sdks/browser/BA.js"
         data-profileUrl="https://collector-axa.cloud.ca.com//api/1/urn:ca:tenantId:CACDNA01-USERSTORE/urn:ca:appId:MSF%20Demo%20West/profile?agent=browser"
         data-tenantID="CACDNA01-USERSTORE"
         data-appID="MSF Demo West"
-        data-appKey="b5a86f10-8cc5-11e7-99e7-6fd9d2039098"></script>
+        data-appKey="b5a86f10-8cc5-11e7-99e7-6fd9d2039098" ></script>
